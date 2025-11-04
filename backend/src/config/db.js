1ef5d157654de598825aba1777e5b3fafca3d9a3
BidhyashreeDahal-11 <bidhyashree.dahal@dcmail.ca>
2025-11-03 20:12:40 -0500

    Add server entry point and MongoDB connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,2 +1,15 @@
 // Handles MongoDB connection using Mongoose.
 // Exports a function that connects to MongoDB Atlas and logs connection status.
+// config/db.js
+
+///_____ Testing Database Connection _______///
+import mongoose from 'mongoose';
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected successfully');
+    } catch (error) {console.error('MongoDB connection failed:', error);
+    process.exit(1);
+}
+}
+export default connectDB;
