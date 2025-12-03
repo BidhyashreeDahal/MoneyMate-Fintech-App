d4a3d2dd552ba8f1efb42b5662141f32abf0c899
BidhyashreeDahal-11 <bidhyashree.dahal@dcmail.ca>
2025-12-02 21:47:04 -0500

    feat(auth): implement full user authentication (signup, login, logout) with JWT & secure cookies

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,3 +1,16 @@
 // Defines routes for authentication.
 // Endpoints: POST /signup, POST /login, POST /logout.
 // Connects to auth.controller.js for handling requests.
+
+import express from 'express';
+import { signup, login ,logout } from '../controllers/auth.controller.js';
+
+const router = express.Router();
+// POST /api/auth/signup → Create a new user account
+router.post('/signup', signup);
+// POST /api/auth/login → Authenticate user and return JWT
+router.post('/login', login);
+
+// POST /api/auth/logout → Log out user by clearing JWT cookie
+router.post('/logout', logout);
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,16 +4,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth.routes.js';
 
 dotenv.config(); // Load environment variables
 const app = express();
 connectDB(); // Connect to MongoDB
 
-// Simple test route`
-app.get("/", (req, res) => {
-  res.send("MoneyMate Backend is Running ");
-});
+
+// Parse JSON bodies
+app.use(express.json());
+
+// Parse cookies
+app.use(cookieParser());
+
+app.use('/api/auth', authRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
