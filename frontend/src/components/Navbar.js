d40533aac5bb8df50cf5cec50b677db3ce3c032b
Bidhyashree <bidhyashree.dahal@dcmail.ca>
2026-01-07 02:21:35 -0500

    Working in the frontend

--- frontend/src/components/Navbar.js
+++
@@ -1,2 +0,0 @@
-//Reusable top navigation bar visible on all pages after login. 
-// It may include the app logo, user info, and a logout button.
--- frontend/src/services/api.js
+++
@@ -1,2 +0,0 @@
-//Central place for all API requests (like loginUser(), fetchDashboardData()). 
-// Will use Axios here so that every component doesn’t need to rewrite request logic.
